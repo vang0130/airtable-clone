ff7d291c7c5728fadeb781f979717cd3d66ec374
va_ng <vanessaw2309@live.com>
2024-10-11 15:07:02 +1100

    workspaecs vercel is back

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// /**
+//  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+//  * for Docker builds.
+//  */
+// await import("./src/env.js");
+
+// /** @type {import("next").NextConfig} */
+// const config = {};
+
+// export default config;
+
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,6 +17,20 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Set-Cookie",
+            value: "__vercel_live_token=value; SameSite=None; Secure",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default config;
